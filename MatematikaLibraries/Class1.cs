1cc16695029c0c69b575c2e433b603dc1ad151e7
KoifishVEVO <149301244+KoifishVEVO@users.noreply.github.com>
2024-05-06 19:59:39 +0700

    Add files via upload

---
+++ MatematikaLibraries/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace MatematikaLibraries
+{
+    public class MatematikaLibraries
+    {
+        public static int FPB(int input1, int input2)
+        {
+            return input2 == 0 ? input1 : FPB(input2, input1 % input2);
+        }
+
+        public static int KPK(int input1, int input2)
+        {
+            return (input1 / FPB(input1, input2)) * input2;
+        }
+
+        public static String Turunan(int[] persamaan)
+        {
+            String result = "";
+            int first_num;
+            for (int i = 0, pangkat = persamaan.Length - 1; i < persamaan.Length - 2; i++, pangkat--)
+            {
+                first_num = persamaan[i] * pangkat;
+                result +=
+                (first_num == 1 ? null : (first_num).ToString(" + #; -#; 0")) +
+                "x^" + (pangkat - 1 == 1 ? null : (pangkat - 1));
+            }
+            return (result[1].ToString() == "+" ||
+            result[1].ToString() == "-" ? result.Substring(3) : result) +
+            persamaan[^2].ToString(" + #; - #;0");
+        }
+
+        public static String Integral(int[] persamaan)
+        {
+            String result = "";
+            int first_num;
+            for (int i = 0, pangkat = persamaan.Length - i - 1;
+            i < persamaan.Length; i++, pangkat--)
+            {
+                first_num = persamaan[i] / (pangkat + 1);
+                result += (first_num == 1 ? null :
+                first_num.ToString(" + #; -#; 0")) + "x^" + (i == persamaan.Length -
+                1 ? null : (pangkat + 1));
+            }
+            return (result[1].ToString() == "+" ||
+            result[1].ToString() == "-") ?
+            result.Substring(3) : result + " + C";
+        }
+    }
+}
---
+++ console/Program.cs
@@ -0,0 +1,20 @@
+﻿using MatematikaLibraries;
+namespace MatematikaLibraries;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("FPB 40,45");
+        Console.WriteLine(MatematikaLibraries.FPB(60, 45));
+
+        Console.WriteLine("KPK 12,8");
+        Console.WriteLine(MatematikaLibraries.KPK(12, 8));
+
+        Console.WriteLine("Turunan dari persamaan x^3+4x^2-12x+9");
+        Console.WriteLine(MatematikaLibraries.Turunan(new int[]{1, 4, -12, 9}));
+
+        Console.WriteLine("Integral dari persamaan 4x^3 + 6x^2 - 12x + 9");
+        Console.WriteLine(MatematikaLibraries.Integral(new int[] {4,6,-12,9}));
+    }
+}
